2c231558a7cda380a5d23878c6c3a9970aed5de1
serserar <serserar@yahoo.es>
2010-02-24 21:46:58 +0000

    add log info

--- JME20_SMD/src/com/base/model/smd/test/TestSMD.java
+++ JME20_SMD/src/com/base/model/smd/test/TestSMD.java
@@ -1,11 +1,8 @@
 package com.base.model.smd.test;
 
-import com.base.model.jme.SmdNode;
+import com.base.model.jme.SMDNode;
 import com.jme.app.AbstractGame.ConfigShowMode;
-
 import com.jme.app.SimpleGame;
-import com.jme.input.ChaseCamera;
-import com.jme.math.Vector3f;
 import com.jme.scene.shape.AxisRods;
 import com.jme.util.BoneDebugger;
 
@@ -15,7 +12,7 @@ import com.jme.util.BoneDebugger;
  */
 public class TestSMD extends SimpleGame {
 
-    private SmdNode node;
+    private SMDNode node;
     AxisRods ejes;
 
     public TestSMD() {
@@ -42,53 +39,60 @@ public class TestSMD extends SimpleGame {
         ejes.setLocalTranslation(0, 0, 0);
         rootNode.attachChild(ejes);
         /*****  PLAYERS  *****/
-        //node = new SmdNode("C:\\JME\\Models\\Zombie\\Zombie.qc", display);
-        node = new SmdNode("C:\\JME\\Models\\player\\eightball\\eightball.qc", display);
-        //node = new SmdNode("C:\\JME\\Models\\player\\Molly\\Molly.qc", "C:\\JME\\Models\\player\\Molly\\Molly.smd","C:\\JME\\Models\\player\\Molly\\null.bmp", display);
-        //node = new SmdNode("C:\\JME\\Models\\player\\nina\\nina.qc", "C:\\JME\\Models\\player\\nina\\nina_new.smd","C:\\JME\\Models\\player\\nina\\Texture.bmp", display);
-        //node = new SmdNode("C:\\JME\\Models\\Hands\\Winchester\\v_winchester.qc", "C:\\JME\\Models\\Hands\\Winchester\\reference.smd","C:\\JME\\Models\\Hands\\Winchester\\null.bmp", display);
-        //node = new SmdNode("C:\\JME\\Models\\player\\edgar\\edgar.qc", "C:\\JME\\Models\\player\\edgar\\gent.smd","C:\\JME\\Models\\player\\edgar\\dm_face.bmp", display);
-        //node = new SmdNode("C:\\JME\\Models\\player\\fatherd\\fatherd.qc", "C:\\JME\\Models\\player\\fatherd\\fd.smd","C:\\JME\\Models\\player\\fatherd\\dm_face.bmp", display);
-        //node = new SmdNode("C:\\JME\\Models\\player\\louis\\louis.qc", "C:\\JME\\Models\\player\\louis\\louis.smd","C:\\JME\\Models\\player\\louis\\dm_face.bmp", display);
-        //node = new SmdNode("C:\\JME\\Models\\Zombie1\\zombie1.qc", "C:\\JME\\Models\\player\\eightball\\eightball.smd","C:\\JME\\Models\\player\\eightball\\null.bmp", display);
-        //node = new SmdNode("C:\\JME\\Models\\player\\british-infantry\\british-infantry.qc", "C:\\JME\\Models\\player\\eightball\\eightball.smd","C:\\JME\\Models\\player\\eightball\\null.bmp", display);
-        //node = new SmdNode("C:\\JME\\Models\\player\\lycan_rebel\\lycan_rebel.qc", "C:\\JME\\Models\\player\\eightball\\eightball.smd","C:\\JME\\Models\\player\\eightball\\null.bmp", display);
-        //node = new SmdNode("C:\\JME\\Models\\player\\arctic\\arctic.qc", "C:\\JME\\Models\\player\\eightball\\eightball.smd","C:\\JME\\Models\\player\\eightball\\null.bmp", display);
-        //node = new SmdNode("C:\\JME\\Models\\player\\axis-inf\\axis-inf.qc", "C:\\JME\\Models\\player\\eightball\\eightball.smd","C:\\JME\\Models\\player\\eightball\\null.bmp", display);
-        //node = new SmdNode("C:\\JME\\Models\\player\\axis-para\\axis-para.qc", "C:\\JME\\Models\\player\\eightball\\eightball.smd","C:\\JME\\Models\\player\\eightball\\null.bmp", display);
-        //node = new SmdNode("C:\\JME\\Models\\player\\brit-inf\\brit-inf.qc", "C:\\JME\\Models\\player\\eightball\\eightball.smd","C:\\JME\\Models\\player\\eightball\\null.bmp", display);
-        //node = new SmdNode("C:\\JME\\Models\\player\\gign\\gign.qc", "C:\\JME\\Models\\player\\eightball\\eightball.smd","C:\\JME\\Models\\player\\eightball\\null.bmp", display);
-        //node = new SmdNode("C:\\JME\\Models\\player\\gsg9\\gsg9.qc", "C:\\JME\\Models\\player\\eightball\\eightball.smd","C:\\JME\\Models\\player\\eightball\\null.bmp", display);
-        //node = new SmdNode("C:\\JME\\Models\\player\\guerilla\\guerilla.qc", "C:\\JME\\Models\\player\\eightball\\eightball.smd","C:\\JME\\Models\\player\\eightball\\null.bmp", display);
-        //node = new SmdNode("C:\\JME\\Models\\player\\leet\\leet.qc", "C:\\JME\\Models\\player\\eightball\\eightball.smd","C:\\JME\\Models\\player\\eightball\\null.bmp", display);
-        //node = new SmdNode("C:\\JME\\Models\\player\\terror\\terror.qc", "C:\\JME\\Models\\player\\eightball\\eightball.smd","C:\\JME\\Models\\player\\eightball\\null.bmp", display);
-        //node = new SmdNode("C:\\JME\\Models\\player\\urban\\urban.qc", "C:\\JME\\Models\\player\\eightball\\eightball.smd","C:\\JME\\Models\\player\\eightball\\null.bmp", display);
-        //node = new SmdNode("C:\\JME\\Models\\player\\us-inf\\us-inf.qc", "C:\\JME\\Models\\player\\eightball\\eightball.smd","C:\\JME\\Models\\player\\eightball\\null.bmp", display);
-        //node = new SmdNode("C:\\JME\\Models\\player\\us-para\\us-para.qc", "C:\\JME\\Models\\player\\eightball\\eightball.smd","C:\\JME\\Models\\player\\eightball\\null.bmp", display);
-        //node = new SmdNode("C:\\JME\\Models\\player\\civleader\\civLeader.qc", "C:\\JME\\Models\\player\\eightball\\eightball.smd","C:\\JME\\Models\\player\\eightball\\null.bmp", display);
-        //node = new SmdNode("C:\\JME\\Models\\player\\alien1\\alien1.qc", "C:\\JME\\Models\\player\\eightball\\eightball.smd","C:\\JME\\Models\\player\\eightball\\null.bmp", display);
-        //node = new SmdNode("C:\\JME\\Models\\player\\alien2\\alien2.qc", "C:\\JME\\Models\\player\\eightball\\eightball.smd","C:\\JME\\Models\\player\\eightball\\null.bmp", display);
-        //node = new SmdNode("C:\\JME\\Models\\player\\alien3\\alien3.qc", "C:\\JME\\Models\\player\\eightball\\eightball.smd","C:\\JME\\Models\\player\\eightball\\null.bmp", display);
-        //node = new SmdNode("C:\\JME\\Models\\player\\alien4\\alien4.qc", "C:\\JME\\Models\\player\\eightball\\eightball.smd","C:\\JME\\Models\\player\\eightball\\null.bmp", display);
-        //node = new SmdNode("C:\\JME\\Models\\player\\alien5\\alien5.qc", "C:\\JME\\Models\\player\\eightball\\eightball.smd","C:\\JME\\Models\\player\\eightball\\null.bmp", display);
+        //node = new SMDNode("C:\\JME\\Models\\Zombie\\Zombie.qc", display);
+        //VAMPIRE SLAYER
+        node = new SMDNode("C:\\JME\\Models\\player\\eightball\\eightball.qc", display);
+        //node = new SMDNode("C:\\JME\\Models\\player\\Molly\\Molly.qc", display);
+        //node = new SMDNode("C:\\JME\\Models\\player\\nina\\nina.qc", display);
+        //node = new SMDNode("C:\\JME\\Models\\Hands\\Winchester\\v_winchester.qc", display);
+        //node = new SMDNode("C:\\JME\\Models\\player\\edgar\\edgar.qc", display);
+        //node = new SMDNode("C:\\JME\\Models\\player\\fatherd\\fatherd.qc", display);
+        //node = new SMDNode("C:\\JME\\Models\\player\\louis\\louis.qc", display);
+        //BRAIN
+        //node = new SMDNode("C:\\JME\\Models\\Zombie1\\zombie1.qc", display);
+        //THE TRENCHES
+        //node = new SMDNode("C:\\JME\\Models\\player\\british-infantry\\british-infantry.qc", display);
+        //UNDERWORLD
+        //node = new SMDNode("C:\\JME\\Models\\player\\lycan_rebel\\lycan_rebel.qc", display);
+        //DAY OF DEFEAT
+        //node = new SMDNode("C:\\JME\\Models\\player\\axis-inf\\axis-inf.qc", display);
+        //node = new SMDNode("C:\\JME\\Models\\player\\axis-para\\axis-para.qc", display);
+        //node = new SMDNode("C:\\JME\\Models\\player\\brit-inf\\brit-inf.qc", display);
+        //node = new SMDNode("C:\\JME\\Models\\player\\us-inf\\us-inf.qc", display);
+        //node = new SMDNode("C:\\JME\\Models\\player\\us-para\\us-para.qc", display);
+        //COUNTER STRIKE
+        //node = new SMDNode("C:\\JME\\Models\\player\\gign\\gign.qc", display);
+        //node = new SMDNode("C:\\JME\\Models\\player\\arctic\\arctic.qc", display);
+        //node = new SMDNode("C:\\JME\\Models\\player\\gsg9\\gsg9.qc", display);
+        //node = new SMDNode("C:\\JME\\Models\\player\\guerilla\\guerilla.qc", display);
+        //node = new SMDNode("C:\\JME\\Models\\player\\leet\\leet.qc", display);
+        node = new SMDNode("C:\\JME\\Models\\player\\terror\\terror.qc", display);
+        //node = new SMDNode("C:\\JME\\Models\\player\\urban\\urban.qc", display);
+        //NATURAL SELECTION
+        //node = new SMDNode("C:\\JME\\Models\\player\\alien1\\alien1.qc", display);
+        //node = new SMDNode("C:\\JME\\Models\\player\\alien2\\alien2.qc", display);
+        //node = new SMDNode("C:\\JME\\Models\\player\\alien3\\alien3.qc", display);
+        //node = new SMDNode("C:\\JME\\Models\\player\\alien4\\alien4.qc", display);
+        //node = new SMDNode("C:\\JME\\Models\\player\\alien5\\alien5.qc", display);
+        
         /*****  ITEMS  *****/
-        //node = new SmdNode("C:\\JME\\Models\\Cars\\dod_jagd_car\\dod_jagd_car.qc", "C:\\JME\\Models\\player\\eightball\\eightball.smd","C:\\JME\\Models\\player\\eightball\\null.bmp", display);
-        //node = new SmdNode("C:\\JME\\Models\\DodTree\\dod_tree1.qc", "C:\\JME\\Models\\player\\eightball\\eightball.smd","C:\\JME\\Models\\player\\eightball\\null.bmp", display);
-        //node = new SmdNode("C:\\JME\\Models\\bottle1\\bottle1.qc", "C:\\JME\\Models\\player\\eightball\\eightball.smd","C:\\JME\\Models\\player\\eightball\\null.bmp", display);
-        //node = new SmdNode("C:\\JME\\Models\\bottle2\\bottle2.qc", "C:\\JME\\Models\\player\\eightball\\eightball.smd","C:\\JME\\Models\\player\\eightball\\null.bmp", display);
-        //node = new SmdNode("C:\\JME\\Models\\chest\\chest.qc", "C:\\JME\\Models\\player\\eightball\\eightball.smd","C:\\JME\\Models\\player\\eightball\\null.bmp", display);
-        //node = new SmdNode("C:\\JME\\Models\\casket\\casket01.qc", "C:\\JME\\Models\\player\\eightball\\eightball.smd","C:\\JME\\Models\\player\\eightball\\null.bmp", display);
-        //node = new SmdNode("C:\\JME\\Models\\statue\\by_statue_snow.qc", "C:\\JME\\Models\\player\\eightball\\eightball.smd","C:\\JME\\Models\\player\\eightball\\null.bmp", display);
-        //node = new SmdNode("C:\\JME\\Models\\xen_tree4_1\\xen_tree4_1.qc", "C:\\JME\\Models\\player\\eightball\\eightball.smd","C:\\JME\\Models\\player\\eightball\\null.bmp", display);
-        //node = new SmdNode("C:\\JME\\Models\\vsghost\\vsghost.qc", "C:\\JME\\Models\\player\\eightball\\eightball.smd","C:\\JME\\Models\\player\\eightball\\null.bmp", display);
-        //node = new SmdNode("C:\\JME\\Models\\vsghost\\vsghost.qc", "C:\\JME\\Models\\player\\eightball\\eightball.smd","C:\\JME\\Models\\player\\eightball\\null.bmp", display);
-        //node = new SmdNode("C:\\JME\\Models\\pissoir\\pissoir.qc", "C:\\JME\\Models\\player\\eightball\\eightball.smd","C:\\JME\\Models\\player\\eightball\\null.bmp", display);
-        //node = new SmdNode("C:\\JME\\Models\\klo\\klo.qc", "C:\\JME\\Models\\player\\eightball\\eightball.smd","C:\\JME\\Models\\player\\eightball\\null.bmp", display);
-        //node = new SmdNode("C:\\JME\\Models\\xen_van3_2\\xen_van3_2.qc", "C:\\JME\\Models\\player\\eightball\\eightball.smd","C:\\JME\\Models\\player\\eightball\\null.bmp", display);
-        //node = new SmdNode("C:\\JME\\Models\\xen_rustcar\\xen_rustcar.qc", "C:\\JME\\Models\\player\\eightball\\eightball.smd","C:\\JME\\Models\\player\\eightball\\null.bmp", display);
-        //node = new SmdNode("C:\\JME\\Models\\xen_car4_2\\xen_car4_2.qc", "C:\\JME\\Models\\player\\eightball\\eightball.smd","C:\\JME\\Models\\player\\eightball\\null.bmp", display);
+//        node = new SMDNode("C:\\JME\\Models\\Cars\\dod_jagd_car\\dod_jagd_car.qc", display);
+//        node = new SMDNode("C:\\JME\\Models\\DodTree\\dod_tree1.qc", display);
+//        node = new SMDNode("C:\\JME\\Models\\bottle1\\bottle1.qc", display);
+//        node = new SMDNode("C:\\JME\\Models\\bottle2\\bottle2.qc", display);
+//        node = new SMDNode("C:\\JME\\Models\\chest\\chest.qc", display);
+//        node = new SMDNode("C:\\JME\\Models\\casket\\casket01.qc", display);
+//        node = new SMDNode("C:\\JME\\Models\\statue\\by_statue_snow.qc", display);
+//        node = new SMDNode("C:\\JME\\Models\\xen_tree4_1\\xen_tree4_1.qc", display);
+//        node = new SMDNode("C:\\JME\\Models\\vsghost\\vsghost.qc", display);
+//        node = new SMDNode("C:\\JME\\Models\\vsghost\\vsghost.qc", display);
+//        node = new SMDNode("C:\\JME\\Models\\pissoir\\pissoir.qc", display);
+//        node = new SMDNode("C:\\JME\\Models\\klo\\klo.qc", display);
+//        node = new SMDNode("C:\\JME\\Models\\xen_van3_2\\xen_van3_2.qc", display);
+//        node = new SMDNode("C:\\JME\\Models\\xen_rustcar\\xen_rustcar.qc", display);
+        //node = new SMDNode("C:\\JME\\Models\\xen_car4_2\\xen_car4_2.qc", display);
+        node.getController().setActiveAnimation("run");
         rootNode.attachChild(node);
-
     }
 
     @Override
